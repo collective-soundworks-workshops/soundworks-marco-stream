7e70f23e9db5dd231759fcd35f1bec477b81f66a
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2017-10-10 13:03:20 +0200

    updated default stream files

--- src/shared/audio-config.js
+++ src/shared/audio-config.js
@@ -1,14 +1,14 @@
 export default {
   streams: [
-    './public/streams/test/stream-0.wav',
-    './public/streams/test/stream-1.wav',
-    './public/streams/test/stream-2.wav',
-    './public/streams/test/stream-3.wav',
-    './public/streams/test/stream-4.wav',
-    './public/streams/test/stream-5.wav',
-    './public/streams/test/stream-6.wav',
-    './public/streams/test/stream-7.wav',
-    './public/streams/test/stream-8.wav',
+    './public/streams/stream-0.wav',
+    './public/streams/stream-1.wav',
+    './public/streams/stream-2.wav',
+    './public/streams/stream-3.wav',
+    './public/streams/stream-4.wav',
+    './public/streams/stream-5.wav',
+    './public/streams/stream-6.wav',
+    './public/streams/stream-7.wav',
+    './public/streams/stream-8.wav',
   ],
   defaultStream: 'stream-4',
 }
